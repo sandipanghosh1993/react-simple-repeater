1b8a9e9827d7c571de78c5d5adbcf5a35f96f4d9
Ghosh <sandipan.ghosh01@sap.com>
2018-04-04 18:47:36 +0530

    added test case

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import expect from 'expect'
-import Enzyme, { shallow, mount } from 'enzyme'
+import Enzyme, { mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Repeater from '../src/index'
 
